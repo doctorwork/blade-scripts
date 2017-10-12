92350b26960ff853dc0257af570e4c87a7f6eeeb
insane <luojie@medlinker.com>
2017-10-12 10:39:33 +0800

    完善请求

--- app/api.js
+++ app/api.js
@@ -3,21 +3,37 @@
 import axios from "axios";
 import _ from "lodash";
 
-let config = {
-	alwaysResolve: false,
-	errcode: 'errcode',
-	errmsg: 'errmsg'
-}
+export const instance = axios.create();
+
+/**
+ * 默认响应拦截
+ * @param  {object} response 响应内容
+ * @return {}          
+ */
+const responseInterceptor = instance.interceptors.response.use((response) => {
+	switch (response.status) {
+		case 200:
+			return response.data;
+			break;
+		default:
+			return response;
+			break;
+	}
+});
 
 /**
  * 公共请求
- * @param  {[type]} method [description]
- * @param  {[type]} url    [description]
- * @param  {[type]} params [description]
- * @return {[type]}        [description]
+ * @param  {string} method 请求方法
+ * @param  {string} url    请求地址
+ * @param  {object params  请求参数
+ * @return {object}      
  */
 function request(method, url, params) {
-	const conf = { url, method };
+	const conf = { 
+		url, 
+		method, 
+		withCredentials: true
+	};
 
 	// 合并请求代码
 	if (method == 'get') {
@@ -27,48 +43,40 @@ function request(method, url, params) {
 	}
 
 	return new Promise((resolve, reject) => {
-		axios(conf)
-	    .then(response => {
-	      if (response.status === 200) {
-          switch (response.data[config.errcode]) {
-            case 0:
-              resolve(response.data.data);
-                break;
-            default:
-            	reject(response.data);
-          }
-	      } else {
-	        reject(response.data);
-	      }
-	    })
-	    .catch((err) => {
-	        reject(err);
-	    });
+		return instance(conf)
+			.then((response) => {
+				resolve(response);
+			});
 	})
-}
-
-/**
- * get 请求
- */
-export function $get (url, params) {
-	return request('get', url, params);
-}
-
-/**
- * post 请求
- */
-export function $post (url, params) {
-	return request('post', url, params);
 }
 
 /**
  * 初始化 axios 配置
  * @return {} [description]
  */
-export function setup (func, opts) {
+export function setup (opts) {
+	const { interceptors } = opts;
+	const defautls = _.omit(opts, 'interceptors');
+
+	if (interceptors && interceptors.response) {
+		instance.interceptors.response.eject(responseInterceptor);
+		instance.interceptors.response.use(interceptors.response);
+	}
+
 	// merge with other options
-	config = _.merge(config, opts);
-	func && func.call(null, axios);
+	instance.defautls =	_.defaultsDeep(defautls, {
+		timeout: 10000,
+		withCredentials: true
+	});
+}
+
+/**
+ * 创建单独的请求对象
+ * @param  {object} options 配置对象
+ * @return {object}         
+ */
+export function createApi(options) {
+	return axios.create(options);
 }
 
 // 替换url中参数
@@ -78,29 +86,51 @@ function parseUrl(url, params) {
   });
 }
 
+function maker(method, url) {
+	return function (data, params) {
+		return request(method, parseUrl(url, params), data);
+	}
+}
+
 // 创建get 请求 路由
 export function makeGet (url) {
-	return function (data, params) {
-		return $get(parseUrl(url, params), data);
-	}
+	return maker('get', url);
 }
 
 // 创建post 请求 路由
 export function makePost (url) {
-	return function (data, params) {
-		return $post(parseUrl(url, params), data);
-	}
+	return maker('post', url);
+}
+
+// 创建post 请求 路由
+export function makePut (url) {
+	return maker('put', url);
+}
+
+// 创建post 请求 路由
+export function makeDelete (url) {
+	return maker('delete', url);
 }
 
 // 创建resource 请求
-export makeResource (url, actions, opts) {
+export function makeResource (url, actions, opts) {
 	var schemas = actions || {
 		get: { method: 'GET', url: url + "/:id" },
 		query: { method: 'GET', url },
+		create: { method: 'POST', url },
 		update: { method: 'PUT', url: url + "/:id" },
 		delete: { method: "DELETE", url: url + "/:id" }
 	}
-	return _.forEach((item, key) => {
-		return item;
+
+	const _makers = {makeGet, makePost, makePut, makeDelete};
+
+	function initial(str) {
+		return str.toLowerCase().replace(/^(\w)/, (m) => { 
+		  return m.toUpperCase()
+		});
+	}
+
+	return _.mapValues(schemas, (item) => {
+		return _makers['make' + initial(item.method)](item.url);
 	})
 }
