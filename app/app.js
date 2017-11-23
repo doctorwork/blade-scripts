90abf79ac97f9e33a088afcc02d6c3a5a78fdea8
daipeng <daipeng@doctorwork.com>
2017-11-23 11:32:06 +0800

    对接sentry客户端

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@
 /*eslint-disable*/
 
 import Vue from "vue";
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
 import { createRouter } from "./router";
 import "./assets/reset"; // 全局样式
 import installPlugins from "./plugins";
@@ -17,6 +19,20 @@ import InstallComponents from "./components";
 <% opts.css.forEach(item => { %>import "<%= item %>";<% }) %>
 <% if (opts.type == 'mobile') { %>// 设置 rem
 document.documentElement.style.fontSize = '20px';<% } %>
+
+<% if(opts.addons.sentry && opts.addons.sentry.host) {%>
+    if(process.env.NODE_ENV === 'production' ){
+        Raven
+        .config('<%= opts.addons.sentry.dns %>',{
+            release : '<%= opts.version %>'
+        })
+        .addPlugin(RavenVue, Vue)
+        .setTagsContext({
+            environment : 'development'
+        })
+        .install();
+    }
+<% } %>
 
 // 插件注入
 Vue.use(InstallComponents);
